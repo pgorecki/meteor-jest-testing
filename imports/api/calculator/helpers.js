df63dcfaa4b60b00105af19e3634d535b3deafcb
Przemysław Górecki <gorecki@ermlab.com>
2018-05-08 13:04:06 +0200

    basic tests with relative imports

---
+++ imports/api/calculator/helpers.js
@@ -0,0 +1,3 @@
+export function debug(x) {
+  console.log(x);
+}
---
+++ imports/api/calculator/methods.js
@@ -0,0 +1,8 @@
+// import debug from '/imports/api/sum/helpers';
+import debug from './helpers';
+
+export function sum(a, b) {
+  console.log(__dirname);
+  return a + b;
+}
+
